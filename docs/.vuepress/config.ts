5ccdd15b6dd84bdd2d929cb2a5e08475126296a0
SerhiiCho <serhiicho@protonmail.com>
2022-12-08 08:03:16 +0200

    Added config and palette.scss

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,50 @@
+import { defineUserConfig, defaultTheme } from 'vuepress'
+import { searchPlugin } from '@vuepress/plugin-search'
+
+export default defineUserConfig({
+    base: '/smooth-loader/',
+    lang: 'en-US',
+    title: 'Smooth loader',
+    description: 'Smooth loader allows you smoothly lazy load images and background images',
+    head: [
+        ['link', { rel: 'icon', href: '/images/favicon.png' }],
+    ],
+    plugins: [
+        searchPlugin({
+            locales: {
+                '/': {
+                    placeholder: 'Search',
+                },
+            },
+        })
+    ],
+    theme: defaultTheme({
+        logo: '/images/logo.png',
+        navbar: [
+            {
+                text: 'Documentation',
+                link: '/documentation',
+            },
+            {
+                text: 'NPM',
+                link: 'https://www.npmjs.com/package/smooth-loader',
+            },
+            {
+                text: 'Usage example',
+                link: 'https://codesandbox.io/s/smooth-loader-example-usage-5xr6h',
+            },
+            {
+                text: 'GitHub',
+                link: 'https://github.com/SerhiiCho/smooth-loader',
+            },
+            {
+                text: '📃 Changelog',
+                link: 'https://github.com/SerhiiCho/smooth-loader/blob/master/CHANGELOG.md',
+            },
+            {
+                text: '👨🏻‍💻 About author',
+                link: 'https://serhii.io/about-me',
+            },
+        ],
+    }),
+})
--- docs/README.md
+++ docs/README.md
@@ -1,3 +1,25 @@
-# Documentation
+---
+home: true
+title: Docs
+heroImage: /images/logo.png
+tagline: Impressive lazy loading for images
+actions:
+- text: Get started →
+  link: /documentation
+  type: primary
+- text: Usage example
+  link: https://codesandbox.io/s/smooth-loader-example-usage-5xr6h
+  type: secondary
+features:
+- title: 🧪 Well tested
+  details: You cannot find datetime package that is tested better than this one. It connected to several code quality analyzers.
+- title: 🇬🇧 Multilingualism
+  details: You can output date in several different languages. You can easily contribute any language that you want in simple 4 steps.
+- title: 💪 Flexible
+  details: Timeago can parse several different date time formats, including UNIX timestamp, date string and time package.
+footer: MIT Licensed | Copyright © 2018 - present Serhii Cho
+---
 
-## Nice one
+Smooth loader allows you smoothly lazy load images and background images. Lazy Loading defers the loading of an image that is not needed on the page immediately. An image, not visible to the user when the page loads, is loaded later when the user scrolls and the image actually becomes visible. If the user never scrolls, an image that is not visible to the user never gets loaded.
+
+This package is a MUST-HAVE for every website containing multiple images on the page. It's surely the best lazy loading package on NPM that you can configure to your own needs.
