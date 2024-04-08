4cbf846b2da5cde7bec2c8376b63dcde4ac5f20e
SerhiiCho <serhiicho@protonmail.com>
2024-04-08 19:21:00 +0300

    Fix mistake in docs

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -2,9 +2,22 @@ import { defineClientConfig } from 'vuepress/client'
 import smoothLoader from 'smooth-loader'
 
 export default defineClientConfig({
+    enhance({ app, router, siteData }) {
+        router.afterEach(() => runSmoothLoader())
+    },
+
     setup() {
-        window.setTimeout(() => {
-            smoothLoader()
-        }, 100)
+        runSmoothLoader()
     },
 })
+
+function runSmoothLoader() {
+    setTimeout(() => {
+        if (typeof smoothLoader === 'function') {
+            smoothLoader()
+            return
+        }
+
+        console.warn('smooth-loader is not found')
+    }, 100)
+}
