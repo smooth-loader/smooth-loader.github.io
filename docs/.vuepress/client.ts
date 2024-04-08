37c2712bdbeed4b1110c05f7f374a7014e861fd3
SerhiiCho <serhiicho@protonmail.com>
2024-04-08 13:31:13 +0300

    Fix mistake

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -2,15 +2,9 @@ import { defineClientConfig } from 'vuepress/client'
 import smoothLoader from 'smooth-loader'
 
 export default defineClientConfig({
-    enhance({ app, router, siteData }) {
-        router.afterEach(() => {
-            smoothLoader()
-        })
-    },
     setup() {
         window.setTimeout(() => {
             smoothLoader()
         }, 100)
     },
-    rootComponents: [],
 })
